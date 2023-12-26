ee852d51e995c3d1e66dac51b635a406dd323186
Hazarlincon52 <72333702+Hazarlincon52@users.noreply.github.com>
2023-12-26 20:38:02 +0700

    Perbaikan UI and Tambahan Credit
    Perbaikan resolusi UI Background dan menambah bagian credit button dan credit section.

--- Assets/Scripts/MainMenuUIControlle.cs
+++ Assets/Scripts/MainMenuUIControlle.cs
@@ -4,17 +4,22 @@ using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
-using Unity.VisualScripting;
 
 public class MainMenuUIControlle : MonoBehaviour
 {
+    public GameObject menu;
+    public GameObject credit;
     public Button playButton;
+    public Button creditButton;
+    public Button returnButton;
     public Button exitButton;
 
     private void Start()
     {
         playButton.onClick.AddListener(PlayGame);
+        creditButton.onClick.AddListener(CreditButton);
         exitButton.onClick.AddListener(ExitButton);
+        returnButton.onClick.AddListener(ReturnButton);
     }
 
     private void PlayGame()
@@ -25,5 +30,17 @@ public class MainMenuUIControlle : MonoBehaviour
     private void ExitButton()
     {
         Application.Quit();
+    }
+
+    private void CreditButton()
+    {
+        menu.SetActive(false);
+        credit.SetActive(true);
+    }
+
+    private void ReturnButton()
+    {
+        menu.SetActive(true);
+        credit.SetActive(false);
     }
 }
